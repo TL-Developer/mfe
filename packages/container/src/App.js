5bf0fcb9f63157ce7a07adac6018236202c81cbb
Tiago Lima <tiago.lima@xpi.com.br>
2022-03-15 19:30:36 -0300

    Fixing prefix css in production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarkeginApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <>
-      <h1>Hi there</h1>
-      <hr />
-      <br />
-      <MarketingApp />
-    </>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
